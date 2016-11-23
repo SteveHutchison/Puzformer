76b7d4a064e43e3a983576ca25e271937afae5ae
SteveHutchison <stevehutchison360@hotmail.co.uk>
2016-11-23 19:56:32 +0000

    Base Project
    Made initial project. Created some basic blocks. Ground blocks and
    bouncy blocks. Created basic player character with simple movement for
    testing.

---
+++ Puzformer/Assets/Bounce.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+    private bool bounce;
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if(bounce == true)
+        {
+            rb2d.AddForce(new Vector2(0, 500));
+            bounce = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "bounce")
+        {
+            bounce = true;
+        }
+    }
+}
---
+++ Puzformer/Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+    Rigidbody2D rb2d;
+    public float horV;
+    public float maxHorVel;
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        horV = rb2d.velocity.x;
+
+        if (Input.GetKey("left") && rb2d.velocity.x > -maxHorVel)
+        {
+            rb2d.AddForce(new Vector3(-10, 0, 0));
+        }
+        if (Input.GetKey("right") && rb2d.velocity.x < maxHorVel)
+        {
+            rb2d.AddForce(new Vector3(10, 0, 0));
+        }
+        if (Input.GetKey("up"))
+        {
+            rb2d.AddForce(new Vector3(0, 50, 0));
+        }
+    }
+}
