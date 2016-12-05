bb2dd21b563dbed6f539e54bcd7706fce06c9aea
SteveHutchison <stevehutchison360@hotmail.co.uk>
2016-12-05 15:37:33 +0000

    bunch of changes
    created prefabs for bouncy and ground objects. Added death blocks which
    currently reset the player to his start position. Collectable objects
    which are removed from the world when collided with. Improved player
    controlls. Added script to make camera follow the player.

---
+++ Puzformer/Assets/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    Transform player;
+    float offsetX;
+    float offsetY;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+
+        player = player_go.transform;
+
+        offsetX = transform.position.x - player.transform.position.x;
+        offsetY = transform.position.y - player.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = player.position.x + offsetX;
+        pos.y = player.position.y + offsetY;
+        transform.position = pos;
+    }
+}
--- Puzformer/Assets/PlayerMove.cs
+++ Puzformer/Assets/PlayerMove.cs
@@ -5,9 +5,13 @@ public class PlayerMove : MonoBehaviour {
     Rigidbody2D rb2d;
     public float horV;
     public float maxHorVel;
+    public bool CanJump;
+    public Vector3 StartP;
 	// Use this for initialization
 	void Start () {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        CanJump = false;
+        StartP = this.transform.position;
     }
 	
 	// Update is called once per frame
@@ -22,9 +26,53 @@ public class PlayerMove : MonoBehaviour {
         {
             rb2d.AddForce(new Vector3(10, 0, 0));
         }
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up") && CanJump)
         {
-            rb2d.AddForce(new Vector3(0, 50, 0));
+            rb2d.AddForce(new Vector3(0, 300, 0));
+            CanJump = false;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Floor")
+        {
+            CanJump = true;
+            //animator.SetBool("jumping", false);
+        }
+
+        if (coll.gameObject.tag == "death")
+        {
+            this.transform.position = StartP;
+            //animator.SetBool("jumping", false);
+        }
+    }
+    /*
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Floor")
+        {
+            CanJump = false;
+            //animator.SetBool("jumping", false);
+        }
+    }
+    */
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        
+        if (coll.gameObject.tag == "Pickup")
+        {
+            Destroy(coll.gameObject);
+        }
+    
+    }
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        /*
+        if (coll.gameObject.tag == "Raising")
+        {
+            
+        }
+        */
     }
 }
